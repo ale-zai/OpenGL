2d6f548d56defa1c8904eab34eefeadf8c8e2027
Aleksei Zaikin <aleksi.zaikin@gmail.com>
2017-01-26 11:09:32 +0300

    Initial Commit

---
+++ OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by Mobile Capital on 26/01/2017.
+//  Copyright © 2017 Mobile Capital. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
